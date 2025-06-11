7463688ad817815b1737ea4a685affe20a58871a
arandintday <combinecore@outlook.com>
2025-06-12 00:11:13 +0800

    ah yes, 3d remake

---
+++ GodotPrj/Script/script_director.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+@onready var camera_3d: Camera3D = $"../../Camera3D"
+
+var gwRotationTimes = 0
+var gwRotationArrow = 0
+
+const ROTATION_OFFSET = 12
+const ROTATION_SPEED = 0.03
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	processingCameraAngle()
+	pass
+
+func processCameraLeft():
+	if(gwRotationTimes < ROTATION_OFFSET):
+		gwRotationTimes += 1
+		moveCameraAngle(ROTATION_SPEED)
+	else:
+		gwRotationTimes = ROTATION_OFFSET
+	pass
+
+func processCameraRight():
+	if(gwRotationTimes > -ROTATION_OFFSET):
+		gwRotationTimes -= 1
+		moveCameraAngle(-ROTATION_SPEED)
+	else:
+		gwRotationTimes = -ROTATION_OFFSET
+	pass
+
+func moveCameraAngle(angle: float):
+	camera_3d.rotate_y(angle)
+	pass
+
+func processingCameraAngle():
+	if gwRotationArrow > 0:
+		processCameraLeft()
+	elif gwRotationArrow < 0:
+		processCameraRight()
+	pass
+
+func onBtnLeftPressed():
+	gwRotationArrow = 1
+	pass
+
+
+func onBtnRightPressed():
+	gwRotationArrow = -1
+	pass
+
+func onBtnLeftRel():
+	gwRotationArrow = 0
+	pass
+
+func onBtnRightRel():
+	gwRotationArrow = 0
+	pass
