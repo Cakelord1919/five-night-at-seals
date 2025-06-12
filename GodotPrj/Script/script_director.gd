b33317520d91bcaf8f9a658412cdaf303dd88e70
arandintday <combinecore@outlook.com>
2025-06-13 01:35:45 +0800

    Add 3d animation and camera angle effect(WIP)

--- GodotPrj/Script/script_director.gd
+++ GodotPrj/Script/script_director.gd
@@ -1,9 +1,36 @@
+'''
+#########################################
+		Variable Name Info
+"g"lobal prefix means global variable
+"o"bject prefix means node object variable
+"b"inary prefix means binary variable
+"w"ord   prefix means numberic variable
+"f"loat  prefix means float variable
+"s"tring prefix means string variable
+lis"t"   prefix means list variable 
+"k"id    preflx means child node object variable
+CapitalFunction() means the function was execute directly
+FULL_CAPITAL means a static numberic 
+#########################################
+'''
+
 extends Node2D
 
-@onready var camera_3d: Camera3D = $"../../Camera3D"
+@onready var ogCamera3D: Camera3D = $"../../Camera3D"
+
+@onready var ogTurnLeftBtn: Button = $"../btnLeft"
+@onready var ogTurnRightBtn: Button = $"../btnRight"
+@onready var ogMapBtn: Button = $"../openMapBtn"
+@onready var ogMonBtn: Button = $"../checkComBtn"
+
+
+@export var okgComAnim: AnimationPlayer
 
 var gwRotationTimes = 0
 var gwRotationArrow = 0
+var gbCheckCom = 0
+var gfCameraDefaultAng = 0
+var gbResetCam = 0
 
 const ROTATION_OFFSET = 17
 const ROTATION_SPEED = 0.03
@@ -15,10 +42,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	processingCameraAngle()
+	ProcessingCameraAngle()
+	ProcessingCameraReset()
+	#print(gwRotationTimes)
 	pass
 
-func processCameraLeft():
+func moveCameraLeft():
 	if(gwRotationTimes < ROTATION_OFFSET):
 		gwRotationTimes += 1
 		moveCameraAngle(ROTATION_SPEED)
@@ -26,7 +55,7 @@ func processCameraLeft():
 		gwRotationTimes = ROTATION_OFFSET
 	pass
 
-func processCameraRight():
+func moveCameraRight():
 	if(gwRotationTimes > -ROTATION_OFFSET):
 		gwRotationTimes -= 1
 		moveCameraAngle(-ROTATION_SPEED)
@@ -35,29 +64,88 @@ func processCameraRight():
 	pass
 
 func moveCameraAngle(angle: float):
-	camera_3d.rotate_y(angle)
+	ogCamera3D.rotate_y(angle)
 	pass
 
-func processingCameraAngle():
+func ProcessingCameraAngle():
 	if gwRotationArrow > 0:
-		processCameraLeft()
+		moveCameraLeft()
 	elif gwRotationArrow < 0:
-		processCameraRight()
+		moveCameraRight()
+	pass
+
+func ProcessingCameraReset():
+	if gbResetCam:
+		cameraReset()
 	pass
 
 func onBtnLeftPressed():
 	gwRotationArrow = 1
 	pass
 
-
 func onBtnRightPressed():
 	gwRotationArrow = -1
 	pass
 
-func onBtnLeftRel():
+func onBtnLeftRelease():
 	gwRotationArrow = 0
 	pass
 
-func onBtnRightRel():
+func onBtnRightRelease():
 	gwRotationArrow = 0
 	pass
+
+func disableAllBtns():
+	ogMonBtn.disabled = true
+	ogMapBtn.disabled = true
+	ogTurnLeftBtn.disabled = true
+	ogTurnRightBtn.disabled = true
+	pass
+
+func enableAllBtns():
+	ogMonBtn.disabled = false
+	ogMapBtn.disabled = false
+	ogTurnLeftBtn.disabled = false
+	ogTurnRightBtn.disabled = false
+	pass
+
+func cameraReset():
+	if gwRotationTimes < 0:
+		gwRotationTimes += 1
+		moveCameraAngle(ROTATION_SPEED)
+		pass
+	if gwRotationTimes > 0:
+		gwRotationTimes -= 1
+		moveCameraAngle(-ROTATION_SPEED)
+		pass
+	if gwRotationTimes == 0:
+		gbResetCam = 0
+		pass
+
+func onCheckComPressed():
+	gbCheckCom = !gbCheckCom
+	gbResetCam = 1
+	processingComputerAnim()
+	pass
+
+func processingComputerAnim():
+	if gbCheckCom:
+		disableAllBtns()
+		ogTurnLeftBtn.visible = false
+		ogTurnRightBtn.visible = false
+		okgComAnim.play("forward")
+	else:
+		disableAllBtns()
+		okgComAnim.play("back")
+	pass
+
+func monAnimFin(anim_name: StringName) -> void:
+	if anim_name == "forward":
+		ogMapBtn.visible = true
+		enableAllBtns()
+	if anim_name == "back":
+		ogTurnLeftBtn.visible = true
+		ogTurnRightBtn.visible = true
+		ogMapBtn.visible = false
+		enableAllBtns()
+	pass # Replace with function body.
