85a994e57758eab16a320fe4a7c3992793304daf
arandintday <combinecore@outlook.com>
2025-06-14 15:18:17 +0800

    Addeed door system control. mesh's albedo viewport bug still persist.

--- GodotPrj/Script/script_director.gd
+++ GodotPrj/Script/script_director.gd
@@ -24,16 +24,28 @@ extends Node2D
 @onready var ogMonBtn: Button = $"../checkComBtn"
 
 
+@export var ogDoorBtnLeft3D: Button
+@export var ogDoorBtnRight3D: Button
 @export var okgComAnim: AnimationPlayer
+@export var ogDoorLeftAnim: AnimationPlayer
+@export var ogDoorRightAnim: AnimationPlayer
 
 var gwRotationTimes = 0
-var gwRotationArrow = 0
+var gwRotationArrow = 0 #1 for left, -1 for right
 var gbCheckCom = 0
 var gfCameraDefaultAng = 0
 var gbResetCam = 0
+var gbDoorLeftStat = 0 #0 for open, 1 for close
+var gbDoorRightStat = 0 #0 for open, 1 for close
 
 const ROTATION_OFFSET = 15
 const ROTATION_SPEED = 0.05
+
+'''
+#########################################
+		Processing Function
+#########################################
+'''
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -46,6 +58,12 @@ func _process(delta: float) -> void:
 	ProcessingCameraReset()
 	#print(gwRotationTimes)
 	pass
+
+'''
+#########################################
+			Sub Function
+#########################################
+'''
 
 func moveCameraLeft():
 	if(gwRotationTimes < ROTATION_OFFSET):
@@ -67,34 +85,6 @@ func moveCameraAngle(angle: float):
 	ogCamera3D.rotate_y(angle)
 	pass
 
-func ProcessingCameraAngle():
-	if gwRotationArrow > 0:
-		moveCameraLeft()
-	elif gwRotationArrow < 0:
-		moveCameraRight()
-	pass
-
-func ProcessingCameraReset():
-	if gbResetCam:
-		cameraReset()
-	pass
-
-func onBtnLeftPressed():
-	gwRotationArrow = 1
-	pass
-
-func onBtnRightPressed():
-	gwRotationArrow = -1
-	pass
-
-func onBtnLeftRelease():
-	gwRotationArrow = 0
-	pass
-
-func onBtnRightRelease():
-	gwRotationArrow = 0
-	pass
-
 func disableAllBtns():
 	ogMonBtn.disabled = true
 	ogMapBtn.disabled = true
@@ -107,6 +97,16 @@ func enableAllBtns():
 	ogMapBtn.disabled = false
 	ogTurnLeftBtn.disabled = false
 	ogTurnRightBtn.disabled = false
+	pass
+
+func disableAllBtns3D():
+	ogDoorBtnLeft3D.disabled = true
+	ogDoorBtnRight3D.disabled = true
+	pass
+
+func enableAllBtns3D():
+	ogDoorBtnLeft3D.disabled = false
+	ogDoorBtnRight3D.disabled = false
 	pass
 
 func cameraReset():
@@ -124,12 +124,6 @@ func cameraReset():
 		processingComputerAnim()
 		pass
 
-func onCheckComPressed():
-	gbCheckCom = !gbCheckCom
-	gbResetCam = 1
-	#processingComputerAnim()
-	pass
-
 func processingComputerAnim():
 	if gbCheckCom:
 		#disableAllBtns()
@@ -141,7 +135,62 @@ func processingComputerAnim():
 		okgComAnim.play("back")
 	pass
 
-func monAnimFin(anim_name: StringName) -> void:
+func processingDoorLeftAnim():
+	disableAllBtns3D()
+	if gbDoorLeftStat:
+		ogDoorLeftAnim.play("down")
+	else:
+		ogDoorLeftAnim.play("lift")
+	pass
+
+func processingDoorRightAnim():
+	disableAllBtns3D()
+	if gbDoorRightStat:
+		ogDoorRightAnim.play("down")
+	else:
+		ogDoorRightAnim.play("lift")
+	pass
+
+'''
+#########################################
+			Function Prime
+#########################################
+'''
+func OnBtnLeftPressed():
+	gwRotationArrow = 1
+	pass
+
+func OnBtnRightPressed():
+	gwRotationArrow = -1
+	pass
+
+func OnBtnLeftRelease():
+	gwRotationArrow = 0
+	pass
+
+func OnBtnRightRelease():
+	gwRotationArrow = 0
+	pass
+
+func OnDoorLeftBtn3DPressed():
+	#print("left!")
+	gbDoorLeftStat =! gbDoorLeftStat
+	processingDoorLeftAnim()
+	pass
+
+func OnDoorRightBtn3DPressed():
+	#print("right!")
+	gbDoorRightStat =! gbDoorRightStat
+	processingDoorRightAnim()
+	pass
+
+func OnCheckComPressed():
+	gbCheckCom = !gbCheckCom
+	gbResetCam = 1
+	#processingComputerAnim()
+	pass
+
+func MonAnimFin(anim_name: StringName) -> void:
 	if anim_name == "forward":
 		ogMapBtn.visible = true
 		enableAllBtns()
@@ -151,3 +200,29 @@ func monAnimFin(anim_name: StringName) -> void:
 		ogMapBtn.visible = false
 		enableAllBtns()
 	pass # Replace with function body.
+
+func DoorLeftAnimFin(anim_name: StringName) -> void:
+	if anim_name == "lift":
+		enableAllBtns3D()
+	if anim_name == "down":
+		enableAllBtns3D()
+	pass # Replace with function body.
+
+func DoorRightAnimFin(anim_name: StringName) -> void:
+	if anim_name == "lift":
+		enableAllBtns3D()
+	if anim_name == "down":
+		enableAllBtns3D()
+	pass # Replace with function body.
+
+func ProcessingCameraAngle():
+	if gwRotationArrow > 0:
+		moveCameraLeft()
+	elif gwRotationArrow < 0:
+		moveCameraRight()
+	pass
+
+func ProcessingCameraReset():
+	if gbResetCam:
+		cameraReset()
+	pass
